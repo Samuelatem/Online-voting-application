3f1c89c100880aa5c2a3bd9c2a9ebc1bc6892a68
Boanerges78 <159068046+Boanerges78@users.noreply.github.com>
2024-04-19 05:31:54 +0100

    Add files via upload
    Candidate and session controllers files

--- candidate.py
+++ candidate.py
@@ -1,65 +1,33 @@
-import sqlite3
+import os
 
-from models.base_model import AbstractBaseModel
+from models.candidate import Candidate
 
-class Candidate(AbstractBaseModel):
-    TABLE_NAME = "candidate"
 
-    def __init__(self, id=None, name=None, picture=None, session=None, position=None, max_vote=None) -> None:
-        self.id = id
-        self.name = name
-        self.picture = picture
-        self.session = session
-        self.position = position
-        self.max_vote = max_vote 
+from werkzeug.utils import secure_filename
 
-    def save(self):
-        if self.id:
-            query = f"UPDATE {self.__class__.TABLE_NAME} SET name=?,picture=?,session=?,position=?,max_vote=? WHERE id=?"
-            
-            with sqlite3.connect("db.sqlite") as connection:
-                cursor = connection.cursor()
-                cursor.execute(query, (self.id, self.name, self.picture, self.session, self.postion, self.max_vote))
-        else:
-            # save into the database
-            query = f"INSERT INTO {self.__class__.TABLE_NAME} (name, picture, session, position,max_vote) VALUES(?,?,?,?)"
-            with sqlite3.connect("db.sqlite") as connection:
-                cursor = connection.cursor()
-                cursor.execute(query, (self.name, self.picture, self.session, self.position,self.max_vote
-                ))
+from constants import UPLOAD_FOLDER
 
-                new_instance_id = cursor.execute(f"SELECT MAX(id) FROM {self.__class__.TABLE_NAME}").fetchone()[0]
+def get_all_candidate():
+    return Candidate.read()
 
-                self.id = new_instance_id
+def get_candidate_with_id(id):
+    return Candidate.read(id)
 
-    def read(id=None):
-        with sqlite3.connect("db.sqlite") as connection:
-            cursor = connection.cursor()
-            if id:
-                query = f"SELECT (id,name, picture, session, position,max_vote) FROM {self.__class__.TABLE_NAME} WHERE id=?"
+def save_candidate(name, picture, session, position,max_vote, id=None, uploaded_files=None):
+    if id != None:
+        candidate = get_candidate_with_id(id)
+        candidate.name, candidate.picture, candidate.session, candidate.position,candidate.max_vote = (
+           name, picture, session, position,max_vote
+        )
+    else:
+       candidate= Candidate(
+            name=name, picture=picture, session=session, position=position, max_vote=max_vote
+        )
 
-                result = cursor.execute(query, (id, )).fetchone()
+    Candidate.save()
 
-                votes = __class__(name=result[1], picture=result[2], session=result[3], position=result[4], max_vote=result[5])
-                votes.id = result[0]
-
-                return votes
-            else:
-                query = f"SELECT (id,name, picture, session, position,max_vote) FROM {self.__class__.TABLE_NAME}"
-                results = cursor.execute(query).fetchall()
-                votes = []
-
-                for result in results:
-                    votes = __class__(name=result[1], picture=result[2], session=result[3], position=result[4], max_vote=result[5])
-                    votes.id = result[0]
-
-                    vote.append(votes)
-                
-                return vote
+    return candidate
     
-    def delete(self):
-        if self.id:
-            with sqlite3.connect("db.sqlite") as connection:
-                cursor = connection.cursor()
-
-                cursor.execute(f"DELETE FROM {self.__class__.TABLE_NAME} WHERE id=?", (self.id, ))
+def delete_candidate(id):
+    Candidate = get_candidate_with_id(id)
+    Candidate.delete()
